8aa9c6e36fcff3c950ad6d3bdac5ce7385b4b6e0
George Stagas <gstagas@gmail.com>
2021-10-29 14:40:47 +0300

    adapt for web-test-runner

--- src/adapter.ts
+++ src/adapter.ts
@@ -1,78 +1,160 @@
-import * as vscode from 'vscode';
-import { TestAdapter, TestLoadStartedEvent, TestLoadFinishedEvent, TestRunStartedEvent, TestRunFinishedEvent, TestSuiteEvent, TestEvent } from 'vscode-test-adapter-api';
-import { Log } from 'vscode-test-adapter-util';
-import { loadFakeTests, runFakeTests } from './fakeTests';
+import * as vscode from "vscode";
+import {
+  RetireEvent,
+  TestAdapter,
+  TestLoadStartedEvent,
+  TestLoadFinishedEvent,
+  TestRunStartedEvent,
+  TestRunFinishedEvent,
+  TestSuiteInfo,
+  TestSuiteEvent,
+  TestEvent,
+} from "vscode-test-adapter-api";
+import { Log } from "vscode-test-adapter-util";
+import { startWebTestRunner } from "./commands";
 
 /**
  * This class is intended as a starting point for implementing a "real" TestAdapter.
  * The file `README.md` contains further instructions.
  */
 export class ExampleAdapter implements TestAdapter {
+  private disposables: { dispose(): void }[] = [];
 
-	private disposables: { dispose(): void }[] = [];
+  private readonly testsEmitter = new vscode.EventEmitter<
+    TestLoadStartedEvent | TestLoadFinishedEvent
+  >();
+  private readonly testStatesEmitter = new vscode.EventEmitter<
+    TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent
+  >();
+  private readonly retireEmitter = new vscode.EventEmitter<RetireEvent>();
+  // private readonly autorunEmitter = new vscode.EventEmitter<void>();
 
-	private readonly testsEmitter = new vscode.EventEmitter<TestLoadStartedEvent | TestLoadFinishedEvent>();
-	private readonly testStatesEmitter = new vscode.EventEmitter<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent>();
-	private readonly autorunEmitter = new vscode.EventEmitter<void>();
+  get tests(): vscode.Event<TestLoadStartedEvent | TestLoadFinishedEvent> {
+    return this.testsEmitter.event;
+  }
+  get testStates(): vscode.Event<
+    TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent
+  > {
+    return this.testStatesEmitter.event;
+  }
+  get retire(): vscode.Event<RetireEvent> {
+    return this.retireEmitter.event;
+  }
 
-	get tests(): vscode.Event<TestLoadStartedEvent | TestLoadFinishedEvent> { return this.testsEmitter.event; }
-	get testStates(): vscode.Event<TestRunStartedEvent | TestRunFinishedEvent | TestSuiteEvent | TestEvent> { return this.testStatesEmitter.event; }
-	get autorun(): vscode.Event<void> | undefined { return this.autorunEmitter.event; }
+  constructor(
+    public readonly workspace: vscode.WorkspaceFolder,
+    private readonly log: Log
+  ) {
+    this.log.info("Initializing WebTestRunner adapter");
 
-	constructor(
-		public readonly workspace: vscode.WorkspaceFolder,
-		private readonly log: Log
-	) {
+    this.disposables.push(this.testsEmitter);
+    this.disposables.push(this.testStatesEmitter);
+    this.disposables.push(this.retireEmitter);
 
-		this.log.info('Initializing example adapter');
+    const writeEmitter = new vscode.EventEmitter<string>();
+    const pty: vscode.Pseudoterminal = {
+      onDidWrite: writeEmitter.event,
+      open: () => {},
+      close: () => {},
+    };
 
-		this.disposables.push(this.testsEmitter);
-		this.disposables.push(this.testStatesEmitter);
-		this.disposables.push(this.autorunEmitter);
+    // let web-test-runner think it's a regular terminal
+    // this allows for watch: true to work
+    process.stdout.isTTY = true;
+    process.stdout.write = (chunk): any => {
+      writeEmitter.fire(chunk.toString().replaceAll("\n", "\r\n"));
+    };
 
-	}
+    const terminal = vscode.window.createTerminal({
+      name: "WebTestRunner",
+      pty,
+    });
 
-	async load(): Promise<void> {
+    // terminal.show();
+  }
 
-		this.log.info('Loading example tests');
+  // TODO: not 'any'
+  private webTestRunner: any;
 
-		this.testsEmitter.fire(<TestLoadStartedEvent>{ type: 'started' });
+  async load(): Promise<void> {
+    this.log.info("Loading WebTestRunner tests");
 
-		const loadedTests = await loadFakeTests();
+    if (this.webTestRunner) {
+      this.disposables.push(this.webTestRunner);
+      await this.webTestRunner.stop();
+    }
 
-		this.testsEmitter.fire(<TestLoadFinishedEvent>{ type: 'finished', suite: loadedTests });
+    this.webTestRunner = await startWebTestRunner({
+      onTestLoadStart: () => {
+        this.testsEmitter.fire(<TestLoadStartedEvent>{ type: "started" });
+      },
 
-	}
+      onTestRunStart: () => {
+        // retire all?
+        this.testStatesEmitter.fire(<TestRunStartedEvent>{
+          type: "started",
+          tests: [],
+        });
+      },
 
-	async run(tests: string[]): Promise<void> {
+      onTestSuiteInfo: (suite: TestSuiteInfo) => {
+        this.testsEmitter.fire(<TestLoadFinishedEvent>{
+          type: "finished",
+          suite,
+        });
 
-		this.log.info(`Running example tests ${JSON.stringify(tests)}`);
+        this.retireAllTests();
+      },
 
-		this.testStatesEmitter.fire(<TestRunStartedEvent>{ type: 'started', tests });
+      onTestEvents: (events: TestEvent[]) => {
+        events.forEach((event) => this.testStatesEmitter.fire(event));
+        this.testStatesEmitter.fire(<TestRunFinishedEvent>{ type: "finished" });
+      },
+    });
+  }
 
-		// in a "real" TestAdapter this would start a test run in a child process
-		await runFakeTests(tests, this.testStatesEmitter);
+  async run(tests: string[]): Promise<void> {
+    this.log.info(`Running example tests ${JSON.stringify(tests)}`);
 
-		this.testStatesEmitter.fire(<TestRunFinishedEvent>{ type: 'finished' });
+    this.webTestRunner.run();
+  }
 
-	}
-
-/*	implement this method if your TestAdapter supports debugging tests
+  /*	implement this method if your TestAdapter supports debugging tests
 	async debug(tests: string[]): Promise<void> {
 		// start a test run in a child process and attach the debugger to it...
 	}
-*/
+	*/
 
-	cancel(): void {
-		// in a "real" TestAdapter this would kill the child process for the current test run (if there is any)
-		throw new Error("Method not implemented.");
-	}
+  /**
+   * Invalidates all the tests for the given files.  This works because the file paths are used ids for the tests suites.
+   * @param testFiles The files to invalidate the results for.
+   */
+  // private retireTestFiles(testFiles: string[]) {
+  //   this.retireEmitter.fire(<RetireEvent>{
+  //     tests: testFiles,
+  //   });
+  // }
 
-	dispose(): void {
-		this.cancel();
-		for (const disposable of this.disposables) {
-			disposable.dispose();
-		}
-		this.disposables = [];
-	}
+  /**
+   * Marks all tests as retired.
+   */
+  private retireAllTests() {
+    this.retireEmitter.fire(<RetireEvent>{});
+  }
+
+  cancel(): void {
+    if (this.webTestRunner) {
+      this.webTestRunner.stop();
+    }
+    // in a "real" TestAdapter this would kill the child process for the current test run (if there is any)
+    // throw new Error("Method not implemented.");
+  }
+
+  dispose(): void {
+    this.cancel();
+    for (const disposable of this.disposables) {
+      disposable.dispose();
+    }
+    this.disposables = [];
+  }
 }
---
+++ src/commands.ts
@@ -0,0 +1,172 @@
+import * as vscode from "vscode";
+
+import { TestSuiteInfo, TestInfo, TestEvent } from "vscode-test-adapter-api";
+
+import {
+  defaultReporter,
+  startTestRunner,
+  TestSession,
+  TestSuiteResult,
+  TestResult,
+} from "@web/test-runner";
+
+interface WebTestRunnerResult {
+  suite: TestSuiteInfo;
+  events: TestEvent[];
+}
+
+const transformSession = (session: TestSession): TestSuiteInfo => {
+  return {
+    type: "suite",
+    id: session.testFile,
+    file: session.testFile,
+    label: session.testFile.split("/").pop() || "",
+    children: [
+      ...(session.testResults?.suites.map(
+        transformSuiteInfo(session.testFile)
+      ) ?? []),
+      ...(session.testResults?.tests.map(transformTestInfo(session.testFile)) ??
+        []),
+    ],
+  };
+};
+
+const transformSuiteInfo =
+  (testFile: string) =>
+  (suite: TestSuiteResult): TestSuiteInfo => {
+    return {
+      type: "suite",
+      id: testFile + suite.name,
+      file: testFile,
+      label: suite.name,
+      children: [
+        ...suite.suites.map(transformSuiteInfo(testFile)),
+        ...suite.tests.map(transformTestInfo(testFile)),
+      ],
+    };
+  };
+
+const transformSuiteResult =
+  (testFile: string) =>
+  (suite: TestSuiteResult): TestEvent[] => {
+    return [
+      ...suite.suites.map(transformSuiteResult(testFile)).flat(),
+      ...suite.tests.map(transformTestResult(testFile)),
+    ];
+  };
+
+const transformTestInfo =
+  (testFile: string) =>
+  (test: TestResult): TestInfo => {
+    return {
+      type: "test",
+      id: testFile + test.name,
+      file: testFile,
+      label: test.name,
+    };
+  };
+
+const transformTestResult =
+  (testFile: string) =>
+  (test: TestResult): TestEvent => {
+    return {
+      type: "test",
+      test: testFile + test.name,
+      ...(!test.error
+        ? {
+            state: "passed",
+            file: testFile,
+          }
+        : {
+            state: "failed",
+            message: test.error.message,
+            tooltip: test.error.message,
+            decorations: [
+              {
+                line: +((test.error.stack ?? "").split(":")[1] ?? "-1") - 1, // 0-based
+                message: test.error.message,
+              },
+            ],
+          }),
+    };
+  };
+
+export const startWebTestRunner = async ({
+  onTestLoadStart,
+  onTestRunStart,
+  onTestSuiteInfo,
+  onTestEvents,
+}: {
+  onTestLoadStart: () => void;
+  onTestRunStart: () => void;
+  onTestSuiteInfo: (suite: TestSuiteInfo) => void;
+  onTestEvents: (events: TestEvent[]) => void;
+}) => {
+  process.chdir(getActiveWorkspaceFolder());
+  const runner = await startTestRunner({
+    autoExitProcess: false,
+    config: {
+      watch: true,
+      reporters: [
+        {
+          start({ config, sessions, testFiles, browserNames, startTime }) {
+            onTestLoadStart();
+          },
+          stop({ sessions, testCoverage, focusedTestFile }) {},
+          onTestRunStarted() {
+            onTestRunStart();
+          },
+          onTestRunFinished({ sessions }) {
+            onTestSuiteInfo(<TestSuiteInfo>{
+              type: "suite",
+              id: "root",
+              label: "WebTestRunner",
+              children: sessions.map(transformSession),
+            });
+
+            onTestEvents(
+              <TestEvent[]>sessions
+                .map((session) =>
+                  transformSuiteResult(session.testFile)(
+                    session.testResults ??
+                      ({
+                        suites: [],
+                        tests: [],
+                      } as unknown as TestSuiteResult)
+                  )
+                )
+                .flat()
+            );
+          },
+        },
+        defaultReporter({
+          reportTestResults: true,
+          reportTestProgress: true,
+        }),
+      ],
+    },
+  });
+
+  if (runner) {
+    return {
+      run: () => {
+        runner.runTests(runner.sessions.all());
+      },
+      stop: () =>
+        new Promise((resolve) => {
+          runner.on("stopped", resolve);
+          runner.stop();
+        }),
+    };
+  } else {
+    return null;
+  }
+};
+
+function getActiveWorkspaceFolder(): string {
+  const uri = vscode.window.activeTextEditor?.document.uri;
+  let fsPath = process.cwd();
+  if (uri)
+    fsPath = vscode.workspace.getWorkspaceFolder(uri)?.uri.fsPath ?? fsPath;
+  return fsPath;
+}
